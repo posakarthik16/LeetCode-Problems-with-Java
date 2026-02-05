b0a986732e7a835a653504fc2d7d71fe055e3d2a
Posa Karthik <posakarthik16@gmail.com>
2026-02-05 12:43:55 +0530

    Leetcode problem - 2011

---
+++ LC2011.java
@@ -0,0 +1,67 @@
+public class LC2011 {
+    public static void main(String[] args){
+
+
+//        Problem-2011
+
+//        There is a programming language with only four operations and one variable X:
+//
+//        ++X and X++ increments the value of the variable X by 1.
+//                --X and X-- decrements the value of the variable X by 1.
+//        Initially, the value of X is 0.
+//
+//        Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+//
+//        Example 1:
+//
+         String[] operations = {"--X","X++","X++"};
+//        Output: 1
+//        Explanation: The operations are performed as follows:
+//        Initially, X = 0.
+//                --X: X is decremented by 1, X =  0 - 1 = -1.
+//        X++: X is incremented by 1, X = -1 + 1 =  0.
+//        X++: X is incremented by 1, X =  0 + 1 =  1.
+//        Example 2:
+//
+//        Input: operations = ["++X","++X","X++"]
+//        Output: 3
+//        Explanation: The operations are performed as follows:
+//        Initially, X = 0.
+//                ++X: X is incremented by 1, X = 0 + 1 = 1.
+//                ++X: X is incremented by 1, X = 1 + 1 = 2.
+//        X++: X is incremented by 1, X = 2 + 1 = 3.
+//        Example 3:
+//
+//        Input: operations = ["X++","++X","--X","X--"]
+//        Output: 0
+//        Explanation: The operations are performed as follows:
+//        Initially, X = 0.
+//        X++: X is incremented by 1, X = 0 + 1 = 1.
+//                ++X: X is incremented by 1, X = 1 + 1 = 2.
+//                --X: X is decremented by 1, X = 2 - 1 = 1.
+//        X--: X is decremented by 1, X = 1 - 1 = 0.
+//
+//
+//        Constraints:
+//
+//        1 <= operations.length <= 100
+//        operations[i] will be either "++X", "X++", "--X", or "X--".
+
+
+        //SOLUTION   Expected Output : 1;
+
+
+        int answer = 0;
+        for(int i=0;i<operations.length;i++){
+            String temp = operations[i];
+
+            if(temp.equals("X++")  ||  temp.equals("++X")){
+                answer++;
+            }else{
+                answer--;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
